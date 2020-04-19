82f12d55ac7bf57ce6c0869d380fec533f873d34
Tao Chen <tc466@cornell.edu>
2020-04-18 17:36:57 -0700

    Enable setting turbo limits.
    The turbo curve can be customized by changing the MSR_TURBO_RATIO_LIMIT
    property in Info.plist.

--- TurboTune/TurboTune.cpp
+++ TurboTune/TurboTune.cpp
@@ -1,1 +1,78 @@
-/* add your code here */
+#include "TurboTune.hpp"
+
+#include <i386/proc_reg.h>
+#include <libkern/libkern.h>
+
+OSDefineMetaClassAndStructors(TurboTune, IOService)
+
+bool TurboTune::init(OSDictionary *dict) {
+    IOLog("TurboTune: Initializing.\n");
+    bool status = IOService::init(dict);
+    
+    if (!status) {
+        IOLog("TurboTune: Initialization failed.\n");
+        return false;
+    }
+    
+    getKextProperties();
+    if (turbo_ratio_limit_ == 0) {
+        IOLog("TurboTune: Invalid MSR_TURBO_RATIO_LIMIT from kext.\n");
+        return false;
+    }
+    
+    return true;
+}
+
+IOService* TurboTune::probe(IOService* provider, SInt32* score) {
+    IOService* service = IOService::probe(provider, score);
+    return service;
+}
+
+bool TurboTune::start(IOService *provider) {
+    IOLog("TurboTune: Starting.\n");
+
+    bool status = IOService::start(provider);
+    if (!status) {
+        IOLog("TurboTune: Start failed.\n");
+        return false;
+    }
+
+    // Back up original turbo ratio limit.
+    original_turbo_ratio_limit_ = getTurboRatioLimit();
+
+    setTurboRatioLimit(turbo_ratio_limit_);
+
+    return true;
+}
+
+void TurboTune::stop(IOService *provider) {
+    IOLog("TurboTune: Stopping.\n");
+
+    // Restore original turbo ratio limit.
+    setTurboRatioLimit(original_turbo_ratio_limit_);
+    
+    IOService::stop(provider);
+}
+
+void TurboTune::free() {
+    IOLog("TurboTune: Cleaning up.");
+    IOService::free();
+}
+
+void TurboTune::getKextProperties() {
+    OSNumber* turbo_ratio_limit = OSDynamicCast(OSNumber,
+        getProperty("MSR_TURBO_RATIO_LIMIT"));
+
+    turbo_ratio_limit_ = turbo_ratio_limit == nullptr ? 0 :
+        turbo_ratio_limit->unsigned64BitValue();
+    IOLog("TurboTune: kext MSR_TURBO_RATIO_LIMIT = 0x%llx\n",
+          turbo_ratio_limit_);
+}
+
+uint64_t TurboTune::getTurboRatioLimit() {
+    return rdmsr64(MSR_TURBO_RATIO_LIMIT);
+}
+
+void TurboTune::setTurboRatioLimit(uint64_t value) {
+    wrmsr64(MSR_TURBO_RATIO_LIMIT, value);
+}
--- TurboTune/TurboTune.hpp
+++ TurboTune/TurboTune.hpp
@@ -1,1 +1,34 @@
-/* add your code here */
+#ifndef TURBOTUNE_HPP_
+#define TURBOTUNE_HPP_
+
+#include <IOKit/IOLib.h>
+#include <IOKit/IOService.h>
+
+class TurboTune : public IOService
+{
+    OSDeclareDefaultStructors(TurboTune)
+
+  public:
+    virtual bool init(OSDictionary *dict) override;
+    IOService* probe(IOService* provider, SInt32* score) override;
+    virtual bool start(IOService *provider) override;
+    virtual void stop(IOService *provider) override;
+    virtual void free(void) override;
+    
+  private:
+    // Read parameters from kext property list.
+    void getKextProperties(void);
+    uint64_t getTurboRatioLimit(void);
+    void setTurboRatioLimit(uint64_t value);
+    
+    // Turbo ratio limit to be set.
+    uint64_t turbo_ratio_limit_;
+    // Original turbo ratio limit.
+    uint64_t original_turbo_ratio_limit_;
+    
+    // Model-Specific Register for Turbo Boost ratio. See Intel Software
+    // Developer's Manual Volume 4: Model-Specific Registers.
+    static const uint32_t MSR_TURBO_RATIO_LIMIT = 0x1ad;
+};
+
+#endif  // TURBOTUNE_HPP_
